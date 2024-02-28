1032c607783e6f3c17958cb13134989895a8b91c
divya18puntambekar <divyapuntambekar7245@gmail.com>
2024-02-28 18:45:40 +0530

    hooks implemented

--- my-app/app/users/[...user]/page.jsx
+++ my-app/app/users/[...user]/page.jsx
@@ -1,12 +1,26 @@
 "use client"
-import { useRouter } from 'next/navigation';
+import { 
+  useRouter,
+  useParams,
+  useSearchParams,
+  usePathname
+} from 'next/navigation';
 
 function userDetails({params}) {
     console.log("=== param s==",params)
     const router = useRouter();
+    const { id } = useParams();
+    const pathName = usePathname();
+    const searchParams = useSearchParams();
+    const name = searchParams.get('name');
+    const age = useSearchParams()
   return (
     <>
       <div>userDetails id</div>
+      <div>id: { id }</div>
+      <div>name: {name}</div>
+      <div>age: {age.get('age')}</div>
+      <div>pathName: {pathName}</div>
       <button onClick={() => router.push('/')}>HomePage</button>
       <button onClick={() => router.push('/dashboard')}>Dashboard</button>
     </>
